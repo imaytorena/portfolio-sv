8711ef95a7028266c72c10df80e303740c23bb5d
Israel Maytorena <i.velazquez@cignuz.com>
2025-04-29 15:42:32 -0600

    :fire: No more badges iframe

--- src/App.svelte
+++ src/App.svelte
@@ -23,8 +23,12 @@
   <div class="section badges">
     <h2 class="gradient-text">Badges & Certifications</h2>
     <div style="display: flex; align-items: center; margin-top: 1rem; gap: 1rem;">
-      <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="790a08df-33ca-410c-86da-d578f86dd959" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
-      <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="c433b5d8-d0bd-4837-991c-558675b8f386" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
+      <a href="https://www.credly.com/badges/790a08df-33ca-410c-86da-d578f86dd959/public_url">
+        <img src="/images/badges/aws-certified-cloud-practitioner.png" />
+      </a>
+      <a href="https://www.credly.com/badges/c433b5d8-d0bd-4837-991c-558675b8f386/public_url">
+        <img src="/images/badges/aws-partner-technical-accredited.png" />
+      </a>
     </div>
   </div>
   <Technologies {technologies} />
